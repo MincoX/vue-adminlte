519fbbc66d9b38ef6625b4e7738c7bbf9c687b4f
MincoX <903444601@qq.com>
2021-12-11 19:40:13 +0800

    perm style

--- src/views/user/UserPerm.vue
+++ src/views/user/UserPerm.vue
@@ -1,174 +1,232 @@
 <template>
-  <section class="content">
+  <section class="content mt-3">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-6">
-
-          <div class="card card-danger">
-            <div class="card-header">
-              <h3 class="card-title">Input masks</h3>
-            </div>
-            <div class="card-body">
-              <!-- Date dd/mm/yyyy -->
-              <div class="form-group">
-                <label>Date masks:</label>
-
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
-                  </div>
-                  <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask="" inputmode="numeric">
-                </div>
-                <!-- /.input group -->
-              </div>
-              <!-- /.form group -->
-
-              <!-- Date mm/dd/yyyy -->
-              <div class="form-group">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
-                  </div>
-                  <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="mm/dd/yyyy" data-mask="" inputmode="numeric">
-                </div>
-                <!-- /.input group -->
-              </div>
-              <!-- /.form group -->
-
-              <!-- phone mask -->
-              <div class="form-group">
-                <label>US phone mask:</label>
-
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                  </div>
-                  <input type="text" class="form-control" data-inputmask="&quot;mask&quot;: &quot;(999) 999-9999&quot;" data-mask="" inputmode="text">
-                </div>
-                <!-- /.input group -->
-              </div>
-              <!-- /.form group -->
-
-              <!-- phone mask -->
-              <div class="form-group">
-                <label>Intl US phone mask:</label>
-
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                  </div>
-                  <input type="text" class="form-control" data-inputmask="'mask': ['999-999-9999 [x99999]', '+099 99 99 9999[9]-9999']" data-mask="" inputmode="text">
-                </div>
-                <!-- /.input group -->
-              </div>
-              <!-- /.form group -->
-
-              <!-- IP mask -->
-              <div class="form-group">
-                <label>IP mask:</label>
-
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="fas fa-laptop"></i></span>
-                  </div>
-                  <input type="text" class="form-control" data-inputmask="'alias': 'ip'" data-mask="" inputmode="numeric">
-                </div>
-                <!-- /.input group -->
-              </div>
-              <!-- /.form group -->
-
-            </div>
-            <!-- /.card-body -->
-          </div>
-
-        </div>
-        <div class="col-md-6">
+        <div class="col-md-3">
           <div class="card card-primary">
             <div class="card-header">
-              <h3 class="card-title">Date picker</h3>
+              <h3 class="card-title">权限列表</h3>
+              <div class="card-tools">
+                <a href class="btn btn-xs btn-primary" @click.prevent="openAddPerm">
+                  <i class="fa fa-plus-circle"></i>添加权限
+                </a>
+              </div>
             </div>
-            <div class="card-body">
-              <!-- Date -->
-              <div class="form-group">
-                <label>Date:</label>
-                <div class="input-group date" id="reservationdate" data-target-input="nearest">
-                  <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate">
-                  <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                  </div>
-                </div>
-              </div>
-              <!-- Date and time -->
-              <div class="form-group">
-                <label>Date and time:</label>
-                <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
-                  <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime">
-                  <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
-                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
-                  </div>
-                </div>
-              </div>
-              <!-- /.form group -->
-              <!-- Date range -->
-              <div class="form-group">
-                <label>Date range:</label>
-
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="far fa-calendar-alt"></i>
-                    </span>
-                  </div>
-                  <input type="text" class="form-control float-right" id="reservation">
-                </div>
-                <!-- /.input group -->
-              </div>
-              <!-- /.form group -->
-
-              <!-- Date and time range -->
-              <div class="form-group">
-                <label>Date and time range:</label>
-
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="far fa-clock"></i></span>
-                  </div>
-                  <input type="text" class="form-control float-right" id="reservationtime">
-                </div>
-              </div>
-
-              <!-- Time range -->
-              <div class="form-group">
-                <label>Time range:</label>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text"><i class="far fa-clock"></i></span>
-                  </div>
-                  <input type="text" class="form-control float-right" id="rangetime">
-                </div>
-              </div>
-
-              <!-- Date and time range -->
-              <div class="form-group">
-                <label>Date range button:</label>
-
-                <div class="input-group">
-                  <button type="button" class="btn btn-default float-right" id="daterange-btn">
-                    <i class="far fa-calendar-alt"></i> Date range picker
-                    <i class="fas fa-caret-down"></i>
-                  </button>
-                </div>
-              </div>
-              <!-- /.form group -->
+            <div class="card-body p-2">
+              <ul class="nav nav-pills flex-column">
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="fa fa-lock"></i>
+                    测试
+                    <i class="fa fa-times-circle text-danger float-right" @click.prevent="delPerm({ 'permId': item.id, 'permName': item.name })"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
-            <div class="card-footer">
-              Visit <a href="https://getdatepicker.com/5-4/">tempusdominus </a> for more examples and information about
-              the plugin.
-            </div>
-            <!-- /.card-body -->
           </div>
         </div>
+        <div class="col-md-4">
+          <div class="card card-warning">
+            <div class="card-header">
+              <h3 class="card-title">角色列表</h3>
+              <div class="card-tools">
+                <a href class="btn btn-xs btn-warning" @click.prevent="openAddRole">
+                  <i class="fa fa-plus-circle"></i>添加角色
+                </a>
+              </div>
+            </div>
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover text-center">
+                <tbody>
+                  <tr>
+                    <th>角色</th>
+                    <th>拥有权限</th>
+                    <th>操作</th>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <a href @click.prevent>
+                        <i class="fas fa-award"></i>
+                        <strong>11</strong>
+                      </a>
+                    </td>
+
+                    <td>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;配置角色</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;重置密码</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;禁用账户</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;激活账户</a>
+                    </td>
+                    <td>
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-warning btn-xs"><i class="fa fa-plus-circle"></i>
+                          配置</button>
+                        <button type="button" class="btn btn-warning btn-xs"><i class="fa fa-times-circle"></i>
+                          删除</button>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a href @click.prevent>
+                        <i class="fas fa-award"></i>
+                        <strong>11</strong>
+                      </a>
+                    </td>
+
+                    <td>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;配置角色</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;重置密码</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;禁用账户</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;激活账户</a>
+                    </td>
+                    <td>
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-warning btn-xs"><i class="fa fa-plus-circle"></i>
+                          配置</button>
+                        <button type="button" class="btn btn-warning btn-xs"><i class="fa fa-times-circle"></i>
+                          删除</button>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="card card-danger">
+            <div class="card-header">
+              <h3 class="card-title">管理员列表</h3>
+              <div class="card-tools">
+                <a href class="btn btn-xs btn-danger" @click.prevent="openAddAdmin">
+                  <i class="fa fa-plus-circle"></i>添加管理员
+                </a>
+              </div>
+            </div>
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover text-center">
+                <tbody>
+                  <tr>
+                    <th>用户名</th>
+                    <th>拥有角色</th>
+                    <th>创建时间</th>
+                    <th>账户状态</th>
+                    <th>操作</th>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <a href @click.prevent>
+                        <i class="fa fa-user-plus"></i>
+                        <span class="ng-binding">
+                          <strong>111</strong>
+                        </span>
+                      </a>
+                    </td>
+                    <td>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;配置角色</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;重置密码</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;禁用账户</a>
+                      <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;激活账户</a>
+                    </td>
+                    <td>333</td>
+                    <!-- <td v-if="item.active" class="text-success"> -->
+                    <td class="text-success">
+                      <strong>
+                        <i class="fas fa-check-circle text-success"></i> 激活
+                      </strong>
+                    </td>
+                    <!-- <td v-if="item.active!=true" class="text-danger">
+                      <strong>
+                        <i class="fa fa-exclamation-circle text-danger"></i> 禁用
+                      </strong>
+                    </td> -->
+
+                    <td>
+
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-danger dropdown-toggle dropdown-icon btn-xs" data-toggle="dropdown" aria-expanded="false">
+                          <i class="fas fa-cog"></i>
+                          操作
+                        </button>
+                        <div class="dropdown-menu" style="">
+                          <a class="dropdown-item" href="#"><i class="text-blue fa fa-user-secret"></i>&nbsp;配置角色</a>
+                          <a class="dropdown-item" href="#"><i class="text-green fa fa-edit"></i>&nbsp;重置密码</a>
+                          <a class="dropdown-item" href="#"><i class="fas fa-times-circle text-danger"></i>&nbsp;禁用账户</a>
+                          <a class="dropdown-item" href="#"><i class="fas fa-check-circle text-success"></i>&nbsp;激活账户</a>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="col-md-5">
+          <div class="card card-danger">
+            <div class="card-header">
+              <h3 class="card-title">管理员列表</h3>
+              <div class="card-tools">
+                <a href class="btn btn-xs btn-danger" @click.prevent="openAddAdmin">
+                  <i class="fa fa-user-plus"></i>添加管理员
+                </a>
+              </div>
+            </div>
+            <div class="card-body p-2">
+              <ul class="nav nav-pills flex-column">
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" id="customCheckbox2" checked="checked" class="custom-control-input">
+                      <label for="customCheckbox2" class="custom-control-label">Custom Checkbox checked</label>
+                    </div>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" id="customCheckbox2" checked="checked" class="custom-control-input">
+                      <label for="customCheckbox2" class="custom-control-label">Custom Checkbox checked</label>
+                    </div>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" id="customCheckbox2" checked="checked" class="custom-control-input">
+                      <label for="customCheckbox2" class="custom-control-label">Custom Checkbox checked</label>
+                    </div>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" id="customCheckbox2" checked="checked" class="custom-control-input">
+                      <label for="customCheckbox2" class="custom-control-label">Custom Checkbox checked</label>
+                    </div>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" id="customCheckbox2" checked="checked" class="custom-control-input">
+                      <label for="customCheckbox2" class="custom-control-label">Custom Checkbox checked</label>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div> -->
+
       </div>
     </div>
   </section>
 </template>
+
+<style scoped>
+</style>
